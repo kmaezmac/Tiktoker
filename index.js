3823697306135a1e3c0b0a898447b8f5ea1c5ed3
ktrmzm <k.maezmac@gmail.com>
2024-05-21 15:00:39 +0900

    twitter削除

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 import express from "express";
-import { TwitterApi } from "twitter-api-v2";
 import process from "process";
 
-// consumer keys - api key
-const appKey = process.env.TWITTER_API_KEY;
-// consumer keys - api key secret
-const appSecret = process.env.TWITTER_API_SECRET;
-const accessToken = process.env.TWITTER_ACCESS_TOKEN;
-const accessSecret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
-
-const client = new TwitterApi({
-    appKey,
-    appSecret,
-    accessToken,
-    accessSecret,
-});
-client.readWrite;
 const app = express();
 
 const tiktok = async (tiktokAmount, tiktokLink, hatenaLink) => {
